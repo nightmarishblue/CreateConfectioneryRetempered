10bf2b77927c5277a49e18caaec0de656e71f655
blue <midnight@nightmarish.blue>
2024-05-14 17:05:34 +0100

    Tweaked the confectionery potion effects (inshallah they will be perfect some day)

--- src/main/java/blue/nightmarish/create_confectionery/CCConstants.java
+++ src/main/java/blue/nightmarish/create_confectionery/CCConstants.java
@@ -16,18 +16,19 @@ public class CCConstants {
     public static final int WHITE_CHOC_COLOR = 0xB07C4F;
     public static final int CARAMEL_COLOR = 0xad5626;
 
-    public static final int STIMULATION_DURATION = 20 * 120;
+    public static final int STIMULATION_DURATION = 20 * 60 * 2;
     public static final int HEALTH_DURATION = 20 * 90;
-    public static final int REST_DURATION = 20 * 60 * 3;
     public static final int SPEED_DURATION = 20 * 60;
-    public static final int RESISTANCE_DURATION = 20 * 15;
+    public static final int REST_DURATION = 20 * 60 * 3;
+    public static final int REGENERATION_DURATION = 20 * 5;
+    public static final int RESISTANCE_DURATION = 20 * 10;
     public static final int HASTE_DURATION = 20 * 60;
 
     public static final List<MobEffectInstance> DARK_CHOC_EFFECTS = List.of(new MobEffectInstance(CCEffects.STIMULATION.get(), STIMULATION_DURATION));
-    public static final List<MobEffectInstance> RUBY_CHOC_EFFECTS = List.of(new MobEffectInstance(MobEffects.HEALTH_BOOST, HEALTH_DURATION));
-    public static final List<MobEffectInstance> WHITE_CHOC_EFFECTS = List.of(new MobEffectInstance(CCEffects.REST.get(), REST_DURATION));
+    public static final List<MobEffectInstance> RUBY_CHOC_EFFECTS = List.of(new MobEffectInstance(MobEffects.HEALTH_BOOST, HEALTH_DURATION, 1));
+    public static final List<MobEffectInstance> WHITE_CHOC_EFFECTS = List.of(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, SPEED_DURATION));
     public static final List<MobEffectInstance> HOT_CHOC_EFFECTS = List.of(new MobEffectInstance((CCEffects.REST.get()), REST_DURATION));
-    public static final List<MobEffectInstance> CARAMEL_EFFECTS = List.of(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, SPEED_DURATION));
+    public static final List<MobEffectInstance> CARAMEL_EFFECTS = List.of(new MobEffectInstance(MobEffects.REGENERATION, REGENERATION_DURATION));
     public static final List<MobEffectInstance> GINGERBREAD_EFFECTS = List.of(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, RESISTANCE_DURATION));
     public static final List<MobEffectInstance> CANDY_CANE_EFFECTS = List.of(new MobEffectInstance(MobEffects.DIG_SPEED, HASTE_DURATION));
 }
