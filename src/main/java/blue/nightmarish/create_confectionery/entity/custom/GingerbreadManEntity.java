a91c668c03253a22e2ef09fe3148b07ea432a0af
blue <midnight@nightmarish.blue>
2024-05-22 15:42:15 +0100

    Gingerbread man is no longer tameable like an animal

--- src/main/java/blue/nightmarish/create_confectionery/entity/custom/GingerbreadManEntity.java
+++ src/main/java/blue/nightmarish/create_confectionery/entity/custom/GingerbreadManEntity.java
@@ -2,15 +2,12 @@ package blue.nightmarish.create_confectionery.entity.custom;
 
 import blue.nightmarish.create_confectionery.CCUtils;
 import blue.nightmarish.create_confectionery.CreateConfectionery;
-import blue.nightmarish.create_confectionery.registry.CCFluidTypes;
 import blue.nightmarish.create_confectionery.registry.CCItems;
 import blue.nightmarish.create_confectionery.registry.CCSounds;
-import com.simibubi.create.AllFluids;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.ItemTags;
@@ -19,15 +16,17 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
+import net.minecraft.world.entity.animal.AbstractGolem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -38,11 +37,10 @@ import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.fluids.FluidType;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Set;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class GingerbreadManEntity extends TamableAnimal {
+public class GingerbreadManEntity extends AbstractGolem {
     // what it counts as when you gnaw on a gingerbread man
     public static final ItemStack CONSUME_ITEM = new ItemStack(CCItems.GINGERBREAD.get());
     // the particles that come out when you take a bite of this mob
@@ -63,9 +61,12 @@ public class GingerbreadManEntity extends TamableAnimal {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
         // 1. this is client side and we can't do anything but guess the outcome
         if (this.level().isClientSide) {
-            boolean flag = this.isOwnedBy(pPlayer) || this.isTame() || this.isFood(itemstack) && !this.isTame();
-            this.repairParticles(itemstack);
-            return flag ? InteractionResult.CONSUME : InteractionResult.PASS;
+//            boolean flag = this.isOwnedBy(pPlayer) || this.isTame() || this.isFood(itemstack) && !this.isTame();
+            if (this.canEat(itemstack)) {
+                this.repairParticles(itemstack);
+                return InteractionResult.CONSUME;
+            }
+            return InteractionResult.PASS;
         }
 
         // 2. that was food you just gave it; try to feed it
@@ -79,38 +80,38 @@ public class GingerbreadManEntity extends TamableAnimal {
             this.gameEvent(GameEvent.EAT, this);
             return InteractionResult.SUCCESS;
         }
-
-        // 2. this is tame and you can issue a command
-        if (this.isOwnedBy(pPlayer)) {
-            InteractionResult interactionresult = super.mobInteract(pPlayer, pHand);
-            // if something else didn't happen, try to change its state
-            if (!interactionresult.consumesAction()) {
-                this.setOrderedToSit(!this.isOrderedToSit());
-                CreateConfectionery.LOGGER.info("f");
-                this.jumping = false;
-                this.navigation.stop();
-                this.setTarget(null);
-                return InteractionResult.SUCCESS;
-            }
-            return interactionresult; // otherwise report failure
-        }
-
-        // 3. this isn't tame and you can try to tame it
-        if (itemstack.is(TAME_ITEMS)) {
-            if (!pPlayer.getAbilities().instabuild) itemstack.shrink(1);
-
-            if (this.random.nextInt(8) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, pPlayer)) {
-                this.tame(pPlayer);
-                this.navigation.stop();
-                this.setTarget(null);
-                this.setOrderedToSit(false); // unlike wolves, the gingerbread man doesn't sit, so he follows instead
-                this.level().broadcastEntityEvent(this, (byte)7);
-            } else {
-                this.level().broadcastEntityEvent(this, (byte)6);
-            }
-
-            return InteractionResult.SUCCESS;
-        }
+//
+//        // 2. this is tame and you can issue a command
+//        if (this.isOwnedBy(pPlayer)) {
+//            InteractionResult interactionresult = super.mobInteract(pPlayer, pHand);
+//            // if something else didn't happen, try to change its state
+//            if (!interactionresult.consumesAction()) {
+//                this.setOrderedToSit(!this.isOrderedToSit());
+//                CreateConfectionery.LOGGER.info("f");
+//                this.jumping = false;
+//                this.navigation.stop();
+//                this.setTarget(null);
+//                return InteractionResult.SUCCESS;
+//            }
+//            return interactionresult; // otherwise report failure
+//        }
+//
+//        // 3. this isn't tame and you can try to tame it
+//        if (itemstack.is(TAME_ITEMS)) {
+//            if (!pPlayer.getAbilities().instabuild) itemstack.shrink(1);
+//
+//            if (this.random.nextInt(8) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, pPlayer)) {
+//                this.tame(pPlayer);
+//                this.navigation.stop();
+//                this.setTarget(null);
+//                this.setOrderedToSit(false); // unlike wolves, the gingerbread man doesn't sit, so he follows instead
+//                this.level().broadcastEntityEvent(this, (byte)7);
+//            } else {
+//                this.level().broadcastEntityEvent(this, (byte)6);
+//            }
+//
+//            return InteractionResult.SUCCESS;
+//        }
         // 4. ???
         return super.mobInteract(pPlayer, pHand);
     }
@@ -155,11 +156,6 @@ public class GingerbreadManEntity extends TamableAnimal {
     }
 
     @Override
-    public AgeableMob getBreedOffspring(ServerLevel pLevel, AgeableMob pOtherParent) {
-        return null;
-    }
-
-    @Override
     protected SoundEvent getAmbientSound() {
         return this.getEatenState().ordinal() < 2 ? CCSounds.GINGERBREAD_MAN_AMBIENT.get() : null;
     }
@@ -179,7 +175,6 @@ public class GingerbreadManEntity extends TamableAnimal {
         return CCSounds.GINGERBREAD_MAN_DEATH.get();
     }
 
-    @Override
     public boolean isFood(ItemStack pStack) {
         return pStack.is(FOOD_ITEMS);
     }
@@ -188,15 +183,6 @@ public class GingerbreadManEntity extends TamableAnimal {
         return this.isFood(stack) && this.getHealth() < this.getMaxHealth();
     }
 
-    @Override
-    public boolean isBaby() {
-        return false;
-    }
-
-    @Override
-    public boolean canFallInLove() {
-        return false;
-    }
 
     @Override
     public Vec3 getLeashOffset(float pPartialTick) {
@@ -207,10 +193,8 @@ public class GingerbreadManEntity extends TamableAnimal {
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(1, new PanicGoal(this, 1.5D));
-        this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 1.2D, 10F, 2F, false));
+//        this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 1.2D, 10F, 2F, false));
         this.goalSelector.addGoal(3, new WaterAvoidingRandomStrollGoal(this, 1D));
-        this.goalSelector.addGoal(4, new OwnerHurtByTargetGoal(this));
-        this.goalSelector.addGoal(5, new OwnerHurtTargetGoal(this));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
         this.goalSelector.addGoal(7, new FloatGoal(this));
         this.goalSelector.addGoal(8, new TemptGoal(this, 1.2D, Ingredient.of(FOOD_ITEMS), false));
