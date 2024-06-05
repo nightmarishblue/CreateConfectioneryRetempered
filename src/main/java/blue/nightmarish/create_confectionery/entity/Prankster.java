6356b5d5be463b73b0016a24a44cf04d0db23735
blue <midnight@nightmarish.blue>
2024-06-05 16:23:51 +0100

    Created interface for pranking behaviour

---
+++ src/main/java/blue/nightmarish/create_confectionery/entity/Prankster.java
@@ -0,0 +1,29 @@
+package blue.nightmarish.create_confectionery.entity;
+
+public interface Prankster {
+    enum Prank {
+//        THROW_EGG,
+        EAT_CAKE;
+//        CLIMB_HEAD;
+
+        static final Prank[] values = values();
+        public static Prank get(int i) {
+            return values[i];
+        }
+        public static int count = values.length;
+    }
+
+    boolean shouldPrank();
+    default boolean shouldPrank(Prank prankType) {
+        return this.getPrankType() == prankType && this.shouldPrank();
+    }
+    default boolean shouldPrank(int prankType) {
+        return this.getPrankType().ordinal() == prankType && this.shouldPrank();
+    }
+
+    void resetPrankDuration();
+
+
+    void resetPrankType();
+    Prank getPrankType();
+}
