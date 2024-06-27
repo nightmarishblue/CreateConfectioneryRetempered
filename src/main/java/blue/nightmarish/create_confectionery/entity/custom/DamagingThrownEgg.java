30190d523e855bc0cfbaa7cdbaf33978b56e63bd
blue <midnight@nightmarish.blue>
2024-06-27 16:10:14 +0100

    DamagingThrownEgg spawns chickens half as often

--- src/main/java/blue/nightmarish/create_confectionery/entity/custom/DamagingThrownEgg.java
+++ src/main/java/blue/nightmarish/create_confectionery/entity/custom/DamagingThrownEgg.java
@@ -4,21 +4,15 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.projectile.ThrownEgg;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class DamagingThrownEgg extends ThrownEgg {
-    public DamagingThrownEgg(EntityType<? extends ThrownEgg> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
-    }
-
     public DamagingThrownEgg(Level pLevel, LivingEntity pShooter) {
         super(pLevel, pShooter);
-    }
-
-    public DamagingThrownEgg(Level pLevel, double pX, double pY, double pZ) {
-        super(pLevel, pX, pY, pZ);
     }
 
     @Override
@@ -38,5 +32,30 @@ public class DamagingThrownEgg extends ThrownEgg {
         }
 
         super.onHitEntity(pResult);
+    }
+
+    @Override
+    protected void onHit(HitResult pResult) {
+        super.onHit(pResult);
+        if (!this.level().isClientSide) {
+            if (this.random.nextInt(16) == 0) {
+                int i = 1;
+                if (this.random.nextInt(64) == 0) {
+                    i = 4;
+                }
+
+                for(int j = 0; j < i; ++j) {
+                    Chicken chicken = EntityType.CHICKEN.create(this.level());
+                    if (chicken != null) {
+                        chicken.setAge(-24000);
+                        chicken.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), 0.0F);
+                        this.level().addFreshEntity(chicken);
+                    }
+                }
+            }
+
+            this.level().broadcastEntityEvent(this, (byte)3);
+            this.discard();
+        }
     }
 }
