59c13358ae558cf96a5aa972a8c476dea547746e
blue <midnight@nightmarish.blue>
2024-07-03 16:42:41 +0100

    DamagingThrownEgg ACTUALLY spawns half as many chickens

--- src/main/java/blue/nightmarish/create_confectionery/entity/custom/DamagingThrownEgg.java
+++ src/main/java/blue/nightmarish/create_confectionery/entity/custom/DamagingThrownEgg.java
@@ -1,44 +1,51 @@
 package blue.nightmarish.create_confectionery.entity.custom;
 
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Chicken;
-import net.minecraft.world.entity.projectile.ThrownEgg;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 
-public class DamagingThrownEgg extends ThrownEgg {
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class DamagingThrownEgg extends ThrowableItemProjectile {
     public DamagingThrownEgg(Level pLevel, LivingEntity pShooter) {
-        super(pLevel, pShooter);
+        super(EntityType.EGG, pShooter, pLevel);
+    }
+
+    /**
+     * Handles an entity event received from a {@link net.minecraft.network.protocol.game.ClientboundEntityEventPacket}.
+     */
+    @Override
+    public void handleEntityEvent(byte pId) {
+        if (pId == 3) {
+            for(int i = 0; i < 8; ++i) {
+                this.level().addParticle(new ItemParticleOption(ParticleTypes.ITEM, this.getItem()), this.getX(), this.getY(), this.getZ(), ((double)this.random.nextFloat() - 0.5D) * 0.08D, ((double)this.random.nextFloat() - 0.5D) * 0.08D, ((double)this.random.nextFloat() - 0.5D) * 0.08D);
+            }
+        }
+
     }
 
     @Override
     protected void onHitEntity(EntityHitResult pResult) {
-        Entity entity = pResult.getEntity();
-
-        Entity shooter = this.getOwner();
-        DamageSource source;
-        source = this.damageSources().thrown(this, shooter);
-        if (shooter instanceof LivingEntity living) {
-            living.setLastHurtMob(entity);
-        }
-
-        if (entity.hurt(source, 0.5f)) {
-            if (entity.getType() == EntityType.ENDERMAN)
-                return;
-        }
-
         super.onHitEntity(pResult);
+        pResult.getEntity().hurt(this.damageSources().thrown(this, this.getOwner()), 0.5F);
     }
 
     @Override
     protected void onHit(HitResult pResult) {
         super.onHit(pResult);
         if (!this.level().isClientSide) {
-            if (this.random.nextInt(16) == 0) {
+            if (this.random.nextInt(32) == 0) {
                 int i = 1;
                 if (this.random.nextInt(64) == 0) {
                     i = 4;
@@ -57,5 +64,10 @@ public class DamagingThrownEgg extends ThrownEgg {
             this.level().broadcastEntityEvent(this, (byte)3);
             this.discard();
         }
+    }
+
+    @Override
+    protected Item getDefaultItem() {
+        return Items.EGG;
     }
 }
